6ff49269fbd8530e5367acdf9a9fb980855c1863
ahmedius2 <ahmedius2@gmail.com>
2018-09-20 11:18:18 +0300

    removed unnecessary files, again

--- Q8Driver/.q8driver.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/hapticpc/Zenom/Q8Driver/q8driver.ko := ld -r -m elf_x86_64 -T ./scripts/module-common.lds --build-id  -o /home/hapticpc/Zenom/Q8Driver/q8driver.ko /home/hapticpc/Zenom/Q8Driver/q8driver.o /home/hapticpc/Zenom/Q8Driver/q8driver.mod.o
